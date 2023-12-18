459aa4d3950ec72f4550b1d0ab0a9ea7e82d2518
7London7 <106383366+7London7@users.noreply.github.com>
2023-12-18 10:50:11 +0300

    Add files via upload

---
+++ middle_str.h
@@ -0,0 +1,12 @@
+#ifndef MIDDLE_STR_H_INCLUDED
+#define MIDDLE_STR_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+bool itc_isDigit(unsigned char c);
+unsigned char itc_toUpper(unsigned char c);
+unsigned char itc_changeCase(unsigned char c);
+bool itc_Compare(string s1, string s2);
+
+
+#endif // MIDDLE_STR_H_INCLUDED
